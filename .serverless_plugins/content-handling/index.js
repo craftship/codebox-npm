3248d60cd160c1062c2d7c6e405a36924ac0d084
Jon Sharratt <jon@craftship.io>
2017-01-29 21:21:20 +0000

    Tidy up content handling plugin spike

--- .serverless_plugins/content-handling/index.js
+++ .serverless_plugins/content-handling/index.js
@@ -16,47 +16,58 @@ class ContentHandling {
     const apigateway = new this.provider.sdk.APIGateway({
       region: this.options.region,
     });
+
     const integrationResponse = {
       statusCode: '200',
     };
 
-    apigateway.getRestApis().promise()
+    apigateway
+    .getRestApis()
+    .promise()
     .then((apis) => {
       integrationResponse.restApiId = apis.items.find(api => api.name === apiName).id;
-    }).then(() => {
+
+      return apigateway
+      .getResources({ restApiId: integrationResponse.restApiId })
+      .promise();
+    })
+    .then((resources) => {
+      const integrationPromises = [];
+
       Object.keys(funcs).forEach((fKey) => {
         funcs[fKey].events.forEach((e) => {
           if (e.http && e.http.contentHandling) {
             integrationResponse.httpMethod = e.http.method.toUpperCase();
             integrationResponse.contentHandling = e.http.contentHandling;
+            integrationResponse.resourceId = resources.items.find(r => r.path === `/${e.http.path}`).id;
 
-            apigateway.getResources({
-              restApiId: integrationResponse.restApiId,
-            }).promise()
-            .then((resources) => {
-              integrationResponse.resourceId = resources.items.find(r => r.path === `/${e.http.path}`).id;
-              return apigateway.putIntegrationResponse(integrationResponse).promise();
-            })
-            .then((result) => {
-              if (result.statusCode === '200') {
-                return apigateway.createDeployment({
-                  stageName: this.options.stage,
-                  restApiId: integrationResponse.restApiId,
-                }).promise();
-              }
-
-              throw new Error(`Could not set ${integrationResponse.contentHandling} for ${e.http.path}, method ${integrationResponse.httpMethod}`);
-            })
-            .then(() => {
-              this.serverless.cli.log('Setup for AWS API Gateway content handling complete.');
-            })
-            .catch((err) => {
-              this.serverless.cli.log(err.message);
-            });
+            integrationPromises
+            .push(apigateway.putIntegrationResponse(integrationResponse).promise());
           }
         });
       });
-    });
+
+      this.serverless.cli.log('Setting up content handling in AWS API Gateway (takes ~1 min)...');
+      return Promise.all(integrationPromises);
+    })
+    // AWS Limit createDeployment: 3 requests per minute per account
+    // 'Too Many Requests', error may occur as serverless calls this endpoint also.
+    // Wait 1 minute to get reliable deployment.
+    // http://docs.aws.amazon.com/apigateway/latest/developerguide/limits.html
+    .then(() => new Promise(resolve => setTimeout(() => resolve(), 60000)))
+    .then(() => {
+      this.serverless.cli.log('Deploying content handling updates to AWS API Gateway...');
+      return apigateway.createDeployment({
+        stageName: this.options.stage,
+        restApiId: integrationResponse.restApiId,
+      }).promise();
+    })
+    .then((result) => {
+      if (result.id) {
+        this.serverless.cli.log('AWS API Gateway Deployed');
+      }
+    })
+    .catch(err => this.serverless.cli.log(err.message));
   }
 }
 
